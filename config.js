f053517068e18874ea27e7df2a62610d67e274cb
vxggangxv <vxggangxv@naver.com>
2019-07-22 10:11:08 +0900

    u

--- config.js
+++ config.js
@@ -2,14 +2,10 @@ module.exports = function() {
 
 	var source       = 'src',
 		development  = 'dist',
-		bower_path   = 'bower_components',
 		remove       = ['.sass-cache', 'dist'],
 
 		// 템플릿 경로
 		template = {
-//			src  : source + '/jade/**/!(_)*.jade',
-//			parts: source + '/jade/**/_*.jade',
-//			dest : development + '/views'
 			src  : source + '/template/**/!(_)*.html',
 			parts: source + '/template/**/_*.html',
 			dest : development + '/views',
@@ -20,68 +16,47 @@ module.exports = function() {
 
 		// Sass 경로
 		sass = {
-			src       : source + '/sass/**/*.{scss,sass}',
-			compassSrc: source + '/sass',
-			dest      : development + '/static/css'
+			src : source + '/sass/**/*.{scss,sass}',
+			// src       : source + '/sass/**',
+			dest: development + '/static/'
+		},
+
+		// Css 경로
+		css = {
+			// src : source + '/static/css/**/*.css',
+			src : source + '/static/**/*.css',
+			dest: development + '/static'
 		},
 
 		// JS 경로
 		js = {
-			src : source + '/js/**/*.js',
-			dest: development + '/static/js'
+			// src : source + '/static/js/**/*.js',
+			src : source + '/static/**/*.js',
+			dest: development + '/static'
 		},
 
-		// Bower 설정
-		bower = {
-			susy: {
-				src: bower_path + '/susy/sass/**',
-				dest: source + '/sass/susy'
-			},
-			breakpoint: {
-				src: bower_path + '/breakpoint-sass/stylesheets/**',
-				dest: source + '/sass/breakpoint'
-			},
-			fontawesome: {
-				src: bower_path + '/fontawesome/**',
-				dest: source + '/sass/fontawesome'
-			},
-			others: {
-				src: [
-					bower_path + '/**',
-					'!' + bower_path + '/{susy, susy/**}',
-					'!' + bower_path + '/{fontawesome, fontawesome/**}',
-					'!' + bower_path + '/{breakpoint-sass,breakpoint-sass/**}'
-				],
-				dest: development + '/js/libs'
-			}
+		img = {
+			src : source + '/img/**/*.{gif,jpg,png}',
+			dest: development + '/img'
 		},
 
-		// HTML Prettify 옵션
-		htmlPrettify = {
-			"indent_char": " ",
-			"indent_size": 4
-		},
-
-		// 웹 서버 설정
-		server = {
-			root: [development],
-			port: 90,
-			livereload: true,
-			open: {
-				browser: 'chrome' // chrome(안되면 Google Chrome), iexplore, ...
-			}
+		// HTML 옵션
+		htmlbeautify = {
+			"indentSize": 4
 		};
 
 	return {
 		del  : remove,
 		src  : source,
 		dev  : development,
-		rev  : remove,
-		sev  : server,
-
-		bower: bower,
+		
 		template : template,
-		sass : sass,
-		js   : js
+		sass     : sass,
+		css      : css,
+		js       : js,
+
+		img  : img,
+
+		htmlbeautify : htmlbeautify
 	};
 };
